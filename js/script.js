cd85fe13e68c2db9caaa4586a8e3d80e1a5f1ad0
marshjaja <114920895+marshjaja@users.noreply.github.com>
2023-12-15 21:13:22 +0000

    feat: Implement initial functionality for fetching data from the API

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-console.log("hello");
+let searchBtn = $("#search-button");
+
+let APIKey = "32cc7390332ed9b5800335391e652255";
+let queryURL = "";
+
+const getWeather = () => {
+	let newName = $("#search-input").val();
+	let cityName = $("#weather-search");
+	cityName.text(newName);
+	queryURL =
+		"https://api.openweathermap.org/data/2.5/forecast?q=" +
+		newName +
+		"&units=metric&cnt=5&appid=" +
+		APIKey;
+};
+
+searchBtn.on("click", function (event) {
+	event.preventDefault();
+	getWeather();
+
+	fetch(queryURL)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			console.log(queryURL);
+			console.log(data);
+		});
+});
