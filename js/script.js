594afba2a1a43165e0566b68d02e50ab902a0116
marshjaja <114920895+marshjaja@users.noreply.github.com>
2023-12-17 01:56:05 +0000

    feat: Implement initial functionality to display data from the API on Bootstrap card

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 let searchBtn = $("#search-button");
-
+let forecastDetails = $("#forecast");
+let todaysDetails = $("#today");
 let APIKey = "32cc7390332ed9b5800335391e652255";
 let queryURL = "";
+let allWeatherData = $("#weather-data");
 
 const getWeather = () => {
 	let newName = $("#search-input").val();
@@ -23,7 +25,53 @@ searchBtn.on("click", function (event) {
 			return response.json();
 		})
 		.then(function (data) {
-			console.log(queryURL);
 			console.log(data);
+			// console.log(data.list[0].wind.speed);
+			// console.log(data.list[0].main.humidity);
+			// console.log(data.list[0].weather[0].icon);
+
+			//?? CITY
+			let city = $("#search-input").val();
+			$("#search-input").val("");
+			let displayCity = $("<h3>").text("Welcome to " + city);
+			displayCity.attr("class", "card-city");
+			todaysDetails.find("#card-city").text(city);
+
+			//?? DAYs ICON
+			let daysIcon = data.list[0].weather[0].icon;
+			let displayIcon = $("<img>");
+			displayIcon.attr(
+				"src",
+				"https://openweathermap.org/img/wn/" + daysIcon + "@4x.png"
+			);
+			todaysDetails.find("#card-img").attr("src", displayIcon.attr("src"));
+
+			//?? DATE
+			let startDate = dayjs();
+			let currentDate = startDate;
+			let formattedDate = currentDate.format("DD-MM-YYYY");
+			let dateElement = $("<p>").html(
+				"(" + "<strong>" + formattedDate + "</strong>" + ")"
+			);
+			todaysDetails.find("#card-date").append(dateElement);
+
+			//?? TEMP
+			let daysTemp = data.list[0].main.temp;
+			let displayTemp = $("<p>").text(
+				"The temperature is: " + daysTemp + " °C"
+			);
+			todaysDetails.find("#card-temp").append(displayTemp);
+
+			//? WIND SPEED
+			let daysWind = data.list[0].wind.speed;
+			displayWind = $("<p>").text("Winds of " + daysWind + " km/h");
+			todaysDetails.find("#card-wind").append(displayWind);
+
+			//?? HUMIDITY
+			let daysHumidity = data.list[0].main.humidity;
+			let displayHumidity = $("<p>").text(
+				"The humidity is: " + daysHumidity + "%"
+			);
+			todaysDetails.find("#card-humidity").append(displayHumidity);
 		});
 });
